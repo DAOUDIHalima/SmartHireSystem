d32be20f01a0d53164dd86a23f59af77a607802d
HALIMA DAOUDI <daoudi.halima@yahoo.com>
2024-08-14 00:19:28 +0100

    job service erreur eureka solve

--- job-service/src/main/java/org/shp/job_service/models/Job.java
+++ job-service/src/main/java/org/shp/job_service/models/Job.java
@@ -1,2 +1,54 @@
-package org.shp.job_service.models;public class Job {
+package org.shp.job_service.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "jobs")
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "location")
+    private String location;
+    @Column(name = "companyName")
+    private String companyName;
+    @Column(name = "employmentType")
+    private String employmentType;
+    @Column(name = "salaryRange")
+    private Long salaryRange;
+    @ElementCollection
+    @CollectionTable(name = "job_requirements", joinColumns = @JoinColumn(name = "job_id"))
+    @Column(name = "requirement")
+    private List<String> requirements;
+    @Column(name = "created_at")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate createdAt;
+    @Column(name = "updated_at")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate updatedAt;
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "status")
+    private String status;
+    @Column(name = "experience_level")
+    private String experienceLevel;
+    @Column(name = "education_level")
+    private String educationLevel;
+
+
+
 }
